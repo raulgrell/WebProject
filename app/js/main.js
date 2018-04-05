87f2c2733d1449292cc004efcf372eec343942c8
raulgrell <raulgrell@gmail.com>
2018-04-05 18:29:25 +0100

    Set up basic interface

---
+++ app/js/main.js
@@ -0,0 +1,86 @@
+const card_collection = [{
+    _id: 0,
+    name: 'Watch TV',
+    location: 'Home',
+    description: 'Catch up on a series!'
+},
+{
+    _id: 1,
+    name: 'Go for a walk',
+    location: 'Town',
+    description: 'Check out the area'
+},
+{
+    _id: 2,
+    name: 'Jog in the park',
+    location: 'Park',
+    description: 'Check out the area'
+},
+{
+    _id: 3,
+    name: 'Go to the gym',
+    location: 'Town',
+    description: 'Catch up on a series!'
+},
+{
+    _id: 4,
+    name: 'Play 3v3 Basketball',
+    location: 'Town',
+    description: 'Check out the area'
+},
+{
+    _id: 5,
+    name: 'Host a LAN Party',
+    location: 'Home',
+    description: 'Check out the area'
+}
+];
+
+const location_collection = [
+    {
+        _id: 0,
+        name: 'Home',
+        description: ''
+    },
+    {
+        _id: 1,
+        name: 'Town',
+        description: ''
+    },
+    {
+        _id: 2,
+        name: 'Park',
+        description: ''
+    }
+]
+
+const event_collection = [
+    {
+        _id: 0,
+        name: 'Tour',
+        description: ''
+    },
+    {
+        _id: 0,
+        name: 'Concert',
+        description: ''
+    }
+]
+
+const player_collection = [
+    {
+        _id: 0,
+        name: 'Raul',
+        description: ''
+    },
+    {
+        _id: 1,
+        name: 'Pedro',
+        description: ''
+    },
+    {
+        _id: 2,
+        name: 'Noddy',
+        description: ''
+    }
+]
